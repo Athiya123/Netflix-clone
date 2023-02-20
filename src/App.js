73ac73b2677eca822f7f744416ea2b49dc3271f1
Athiya Fathima <85676437+Athiya123@users.noreply.github.com>
2023-02-21 01:05:27 +0530

    step4

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import Banner from "./components/Banner/Banner";
+import Row from "./components/Row/Row";
+import requests from "./components/Banner/requests";
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Banner/>
+      <Row title={"NETFLIX ORIGINALS"} fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+      <Row title={"TRENDING MOVIES"} fetchUrl={requests.fetchTrending} isLargeRow/>
+      <Row title={"HISTORY MOVIES"} fetchUrl={requests.fetchHistoryMovies} isLargeRow/>
+      <Row title={"ANIMATION MOVIES"} fetchUrl={requests.fetchAnimationMovies} isLargeRow/>
+      <Row title={"FANTASY MOVIES"} fetchUrl={requests.fetchFantasyMovies} isLargeRow/>
+      <Row title={"ROMANCE MOVIES"} fetchUrl={requests.fetchRomanceMovies} isLargeRow/>
     </div>
   );
 }
